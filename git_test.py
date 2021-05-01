d16adc93df90fe81e5fd0f939b0b0bf706913836
larry1121 <78005200+larry1121@users.noreply.github.com>
2021-05-02 00:47:26 +0900

    Update git_test.py

--- git_test.py
+++ git_test.py
@@ -1,1 +1,8 @@
 print("test")
+#mkdir gittest 로 깃폴더생성
+#cd gittest 로 gittest 폴더로이동
+#git init으로 git 시작
+#git_test.py파일을 깃폴더에 넣고 index에 추가하기위해 git add git_test.py
+#git remote -v로 연결된 원격저장소와 상태확인
+#git remote add origin url (name,url)로 원격저장소연경
+#git push origin master로 리포지터리에 추가
